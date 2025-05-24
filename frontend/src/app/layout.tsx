1c3e1f71584f695e19a2747bf39d7d076dfb4f39
Jeffery Harrell <jefferyharrell@gmail.com>
2025-05-24 07:20:40 -0700

    feat: update font settings in globals.css and add Typekit stylesheet link in layout.tsx

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -26,6 +26,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="stylesheet" href="https://use.typekit.net/vxg4wbp.css" />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable} font-sans`}>
         <StytchProvider>{children}</StytchProvider>
         <Toaster position="top-right" />
